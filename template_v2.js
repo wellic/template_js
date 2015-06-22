b98a7c17660d74ca96fc0a55c91731fa71282e52
Valerii Savchenko <valerii.s@giantleaplab.com>
2015-06-22 13:07:44 +0300

    Added modules

--- template_v2.js
+++ template_v2.js
@@ -1,23 +1,4 @@
 !(function () {
-    var fncache = { };
-    var parseTemplate = function (id, data) {
-        var cacheId = 't1_' + id;
-        if ( !fncache[cacheId] ) {
-            fncache[cacheId] = new Function("o",
-                "var p=[],print=function(){p.push.apply(p,arguments);};"
-                + "with(o){p.push('" +
-                document.getElementById(id).innerHTML
-                .replace(/[\r\t\n]/g, " ")
-                .replace(/'(?=[^#]*#>)/g, "\t")
-                .split("'").join("\\'")
-                .split("\t").join("'")
-                .replace(/<#=(.+?)#>/g, "',$1,'")
-                .split("<#").join("');")
-                .split("#>").join("p.push('")
-                + "');}return p.join('');");
-        }
-        return data ? (fncache[cacheId])(data) : fncache[cacheId];
-    };
 
     var o = {
         header : "test1",
@@ -26,7 +7,7 @@
 
     document.getElementById("results").innerHTML = "<hr />";
 
-    var t1 = parseTemplate('template');
+    var t1 = mTmpl2.parseTemplate('template');
     document.getElementById("results").innerHTML += t1(o) + "<hr />";
     document.getElementById("results").innerHTML += t1(
         {
@@ -34,7 +15,7 @@
             list : ["l1", "l2", "l3"]
         }) + "<hr />";
 
-    document.getElementById("results").innerHTML += parseTemplate('template',
+    document.getElementById("results").innerHTML += mTmpl2.parseTemplate('template',
         {
             header : "test3",
             list : ["l1", "l2", "l3"]
